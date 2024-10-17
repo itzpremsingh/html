bc4e6c0884511039a2f951f811fb2f29d40aa355
Prem Singh <itzpremsingh@duck.com>
2024-10-17 20:45:35 +0530

    Updating script.js

--- mines-game/script.js
+++ mines-game/script.js
@@ -1,16 +1,16 @@
 const numberOfBoxes = 9;
-const numberOfBombs = 2; // Set number of bombs to 3
+const numberOfBombs = 2;
 let score = 0;
 let bombs = [];
 let mines = [];
 let gameOver = false;
-let clickedBoxes = 0; // Count of clicked boxes
+let clickedBoxes = 0;
 
 function initializeGame() {
   const gameBoard = document.getElementById("game-board");
   gameBoard.innerHTML = "";
   score = 0;
-  clickedBoxes = 0; // Reset clicked boxes count
+  clickedBoxes = 0;
   document.getElementById("score-value").innerText = score;
   gameOver = false;
 
@@ -18,7 +18,6 @@ function initializeGame() {
   mines = [];
   const randomMines = new Set();
 
-  // Randomly place bombs and mines
   while (randomMines.size < numberOfBombs) {
     randomMines.add(Math.floor(Math.random() * numberOfBoxes));
   }
@@ -44,26 +43,23 @@ function handleBoxClick(event) {
   const index = parseInt(box.getAttribute("data-index"));
 
   if (mines.includes(index)) {
-    box.innerHTML = "💣"; // Bomb icon
-    box.style.backgroundColor = "#F44336"; // Set bomb background color to red
+    box.innerHTML = "💣";
+    box.style.backgroundColor = "#F44336";
     showGameOverPopup(false);
   } else {
     if (!box.classList.contains("clicked")) {
-      // Check if box is already clicked
       box.classList.add("clicked");
-      box.innerHTML = "💰"; // Gold mine icon
+      box.innerHTML = "💰";
       score++;
-      clickedBoxes++; // Increment clicked boxes count
+      clickedBoxes++;
       document.getElementById("score-value").innerText = score;
 
-      // Check for victory
       checkVictory();
     }
   }
 }
 
 function checkVictory() {
-  // Check if clicked boxes are all the gold mines
   if (clickedBoxes === numberOfBoxes - numberOfBombs) {
     showGameOverPopup(true);
   }
@@ -79,7 +75,7 @@ function showGameOverPopup(victory) {
     popupMessage.innerText = "Congratulations! You won!";
   } else {
     popupMessage.innerText = "Game Over! You hit a bomb!";
-    // Reveal all bombs
+
     revealBombs();
   }
 }
@@ -87,8 +83,8 @@ function showGameOverPopup(victory) {
 function revealBombs() {
   const boxes = document.querySelectorAll(".box");
   mines.forEach((index) => {
-    boxes[index].innerHTML = "💣"; // Bomb icon
-    boxes[index].style.backgroundColor = "#F44336"; // Red for bomb
+    boxes[index].innerHTML = "💣";
+    boxes[index].style.backgroundColor = "#F44336";
   });
 }
 
@@ -97,5 +93,4 @@ function restartGame() {
   initializeGame();
 }
 
-// Initialize the game when the page loads
 initializeGame();
